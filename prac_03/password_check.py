13d7aff1604e03d0328b0457c53f3a7b70d35d99
naoki-da-costa <naoki.dacosta@my.jcu.edu.au>
2020-08-22 23:07:32 +1000

    "Refactor password check program to use functions"

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,6 +1,21 @@
 PASSWORD_MIN = 5
-password = input("Password: ")
-while len(password) < PASSWORD_MIN:
-    print("Password must be minimum {} characters".format(PASSWORD_MIN))
+
+
+def main():
+    password = get_password(PASSWORD_MIN)
+    print_password(password)
+
+
+def print_password(password):
+    print(len(password) * "*")
+
+
+def get_password(PASSWORD_MIN):
     password = input("Password: ")
-print(len(password) * "*")
+    while len(password) < PASSWORD_MIN:
+        print("Password must be minimum {} characters".format(PASSWORD_MIN))
+        password = input("Password: ")
+    return password
+
+
+main()
