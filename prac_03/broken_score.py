717a8dc4ff49aa007dde7df03d0793c4b7e310fb
naoki-da-costa <naoki.dacosta@my.jcu.edu.au>
2020-08-22 23:14:39 +1000

    "Refactor broken score program to use functions"

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -3,13 +3,23 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-if 0 <= score <= 100:
-    if score >= 90:
-        print("Excellent")
-    elif score >= 50:
-        print("Passable")
-    elif score >= 0:
-        print("Bad")
-else:
-    print("Invalid score")
+
+def main():
+    score = float(input("Enter score: "))
+    result = determine_score(score)
+    print(result)
+
+
+def determine_score(score):
+    if 0 <= score <= 100:
+        if score >= 90:
+            return "Excellent"
+        elif score >= 50:
+            return "Passable"
+        elif score >= 0:
+            return "Bad"
+    else:
+        return "Invalid score"
+
+
+main()
